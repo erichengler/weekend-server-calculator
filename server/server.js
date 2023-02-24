ecaa151a06bd0c005a1d242989c4fcee9593211c
Erich Engler <erich.engler@hotmail.com>
2023-02-24 03:32:30 -0600

    calculator works on the server. still trying to figure out how to get the answer back to the client to display below

--- server/server.js
+++ server/server.js
@@ -5,19 +5,36 @@ app.use(express.json());
 
 // Array to store inputs and calc type
 let calcInputArray = [];
+let currentAnswer;
 
 // GET request for input data and calculation type
 app.get( '/history', (req, res) => {
     console.log( 'GET request made for /history' );
     res.send( calcInputArray );
-} )
+});
 
-// POST request for input data and calculation type in history
+// POST request for input data and operation type in history
 app.post( '/history', (req, res) => {
     console.log( 'POST request made for /history' );
     let currentCalc = req.body;
+
+    // Calculator logic based on input numbers and operation type
+    if ( currentCalc.calcType === '+' ) {
+        currentAnswer = Number(currentCalc.input1) + Number(currentCalc.input2);
+    } else if ( currentCalc.calcType === '-' ) {
+        currentAnswer = Number(currentCalc.input1) - Number(currentCalc.input2);
+    } else if ( currentCalc.calcType === '*' ) {
+        currentAnswer = Number(currentCalc.input1) * Number(currentCalc.input2);
+    } else if ( currentCalc.calcType === '/' ) {
+        currentAnswer = Number(currentCalc.input1) / Number(currentCalc.input2);
+    }
+    console.log( currentAnswer );
+    currentCalc.answer = currentAnswer;
+
     calcInputArray.push( currentCalc );
-})
+    console.log( calcInputArray );
+    res.sendStatus(201);
+});
 
 
 
