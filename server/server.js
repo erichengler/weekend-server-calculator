abf723b4cfec782f9c00cab2eb7fb90c92ff112e
Erich Engler <erich.engler@hotmail.com>
2023-02-24 03:09:56 -0600

    i think im putting too much on the client and not enough on the server.

--- server/server.js
+++ server/server.js
@@ -6,17 +6,20 @@ app.use(express.json());
 // Array to store inputs and calc type
 let calcInputArray = [];
 
-// Request functions go here
+// GET request for input data and calculation type
 app.get( '/history', (req, res) => {
     console.log( 'GET request made for /history' );
     res.send( calcInputArray );
 } )
 
+// POST request for input data and calculation type in history
 app.post( '/history', (req, res) => {
     console.log( 'POST request made for /history' );
     let currentCalc = req.body;
     calcInputArray.push( currentCalc );
 })
+
+
 
 app.use(express.static('server/public'));
 app.listen(port, () => {
